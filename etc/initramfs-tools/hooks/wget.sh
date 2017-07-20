933597423a3dfcd8fa02eeb9b866079aa8dc27cc
Adam Watkins <adam@stupidpupil.co.uk>
2017-07-20 21:59:38 +0100

    Try to make the wget hook architecture-invariant

--- etc/initramfs-tools/hooks/wget.sh
+++ etc/initramfs-tools/hooks/wget.sh
@@ -7,32 +7,36 @@ esac
 
 copy_exec /usr/bin/wget
 
+ARCH=$(dpkg-architecture -qDEB_HOST_MULTIARCH)
+
 # A mindlessly compiled list produced by running `strace wget …`
 # to ensure DNS and SSL support
-copy_exec /usr/lib/i386-linux-gnu/libnettle.so.4
-copy_exec /usr/lib/i386-linux-gnu/libgnutls-deb0.so.28
-copy_exec /lib/i386-linux-gnu/libz.so.1
-copy_exec /usr/lib/i386-linux-gnu/libpsl.so.0
-copy_exec /usr/lib/i386-linux-gnu/libidn.so.11
-copy_exec /lib/i386-linux-gnu/libuuid.so.1
-copy_exec /lib/i386-linux-gnu/libc.so.6
-copy_exec /usr/lib/i386-linux-gnu/libp11-kit.so.0
-copy_exec /usr/lib/i386-linux-gnu/libtasn1.so.6
-copy_exec /usr/lib/i386-linux-gnu/libhogweed.so.2
-copy_exec /usr/lib/i386-linux-gnu/libgmp.so.10
-copy_exec /usr/lib/i386-linux-gnu/libicuuc.so.52
-copy_exec /usr/lib/i386-linux-gnu/libffi.so.6
-copy_exec /lib/i386-linux-gnu/libdl.so.2
-copy_exec /lib/i386-linux-gnu/libpthread.so.0
-copy_exec /usr/lib/i386-linux-gnu/libicudata.so.52
-copy_exec /usr/lib/i386-linux-gnu/libstdc++.so.6
-copy_exec /lib/i386-linux-gnu/libm.so.6
-copy_exec /lib/i386-linux-gnu/libgcc_s.so.1
+copy_exec /lib/$ARCH/libc.so.6
+copy_exec /lib/$ARCH/libdl.so.2
+copy_exec /lib/$ARCH/libgcc_s.so.1
+copy_exec /lib/$ARCH/libm.so.6
+copy_exec /lib/$ARCH/libnss_dns.so.2
+copy_exec /lib/$ARCH/libnss_files.so.2
+copy_exec /lib/$ARCH/libpthread.so.0
+copy_exec /lib/$ARCH/libresolv.so.2
+copy_exec /lib/$ARCH/libuuid.so.1
+copy_exec /lib/$ARCH/libz.so.1
+
+copy_exec /usr/lib/$ARCH/libffi.so.6
+copy_exec /usr/lib/$ARCH/libgmp.so.10
+copy_exec /usr/lib/$ARCH/libgnutls-deb0.so.28
+copy_exec /usr/lib/$ARCH/libhogweed.so.2
+copy_exec /usr/lib/$ARCH/libicudata.so.52
+copy_exec /usr/lib/$ARCH/libicuuc.so.52
+copy_exec /usr/lib/$ARCH/libidn.so.11
+copy_exec /usr/lib/$ARCH/libnettle.so.4
+copy_exec /usr/lib/$ARCH/libp11-kit.so.0
+copy_exec /usr/lib/$ARCH/libpsl.so.0
+copy_exec /usr/lib/$ARCH/libstdc++.so.6
+copy_exec /usr/lib/$ARCH/libtasn1.so.6
+
+copy_exec /usr/lib/$ARCH/gconv/gconv-modules.cache
 copy_exec /usr/lib/locale/locale-archive
-copy_exec /usr/lib/i386-linux-gnu/gconv/gconv-modules.cache
-copy_exec /lib/i386-linux-gnu/libnss_files.so.2
-copy_exec /lib/i386-linux-gnu/libnss_dns.so.2
-copy_exec /lib/i386-linux-gnu/libresolv.so.2
 
 # To allow the HTTPS cert to be verified
 copy_exec /etc/ssl/certs/ca-certificates.crt
