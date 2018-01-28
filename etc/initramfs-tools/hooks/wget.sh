4f6515823ac2f8659a5eb7ea4dde81dd56635790
Adam Watkins <adam@stupidpupil.co.uk>
2018-01-28 00:33:53 +0000

    strace wget on an https site in the hook itself
    This should ensure that the hook works regardless of architecture or changes to wget's dependencies when fetching over https.

--- etc/initramfs-tools/hooks/wget.sh
+++ etc/initramfs-tools/hooks/wget.sh
@@ -7,36 +7,19 @@ esac
 
 copy_exec /usr/bin/wget
 
-ARCH=$(dpkg-architecture -qDEB_HOST_MULTIARCH)
+LIB_PAT="\".*/lib/.*\""
+STRACED_LIBS=$(strace /usr/bin/wget --no-iri -q -O - https://www.debian.org 2>&1 | grep -o "$LIB_PAT")
 
-# A mindlessly compiled list produced by running `strace wget …`
-# to ensure DNS and SSL support
-copy_exec /lib/$ARCH/libc.so.6
-copy_exec /lib/$ARCH/libdl.so.2
-copy_exec /lib/$ARCH/libgcc_s.so.1
-copy_exec /lib/$ARCH/libm.so.6
-copy_exec /lib/$ARCH/libnss_dns.so.2
-copy_exec /lib/$ARCH/libnss_files.so.2
-copy_exec /lib/$ARCH/libpthread.so.0
-copy_exec /lib/$ARCH/libresolv.so.2
-copy_exec /lib/$ARCH/libuuid.so.1
-copy_exec /lib/$ARCH/libz.so.1
+echo "$STRACED_LIBS" | while IFS= read -r line
+  do
+  # Strip the quotation marks
+  line="${line%\"}"
+  line="${line#\"}"
 
-copy_exec /usr/lib/$ARCH/libffi.so.6
-copy_exec /usr/lib/$ARCH/libgmp.so.10
-copy_exec /usr/lib/$ARCH/libgnutls-deb0.so.28
-copy_exec /usr/lib/$ARCH/libhogweed.so.2
-copy_exec /usr/lib/$ARCH/libicudata.so.52
-copy_exec /usr/lib/$ARCH/libicuuc.so.52
-copy_exec /usr/lib/$ARCH/libidn.so.11
-copy_exec /usr/lib/$ARCH/libnettle.so.4
-copy_exec /usr/lib/$ARCH/libp11-kit.so.0
-copy_exec /usr/lib/$ARCH/libpsl.so.0
-copy_exec /usr/lib/$ARCH/libstdc++.so.6
-copy_exec /usr/lib/$ARCH/libtasn1.so.6
+  if [ -f "$line" ]
+  then
+    copy_exec "$line"
+  fi
+done
 
-copy_exec /usr/lib/$ARCH/gconv/gconv-modules.cache
-copy_exec /usr/lib/locale/locale-archive
-
-# To allow the HTTPS cert to be verified
 copy_exec /etc/ssl/certs/ca-certificates.crt
