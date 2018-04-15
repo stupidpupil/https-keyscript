8211acae32d1b33b95cb2a4af6289a246277799f
Adam Watkins <adam@voskhod>
2018-04-15 13:22:45 +0100

    Add test script

---
+++ tests/test.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+correctCsum="$(sha256sum tests/fixtures/unencrypted_keyfile | cut -d " " -f1)"
+
+passphrase="somepassphrase"
+url="https://raw.githubusercontent.com/stupidpupil/https-keyscript/master/tests/fixtures/encrypted_keyfile?$(date +%s)"
+
+export CRYPTTAB_KEY="$passphrase:$url"
+export CRYPTTAB_NAME="TestDevice"
+
+
+runTest()
+{
+  output="$(busybox sh lib/cryptsetup/scripts/wget_or_ask)"
+  csum="$(echo "$output" | sha256sum | cut -d " " -f1)"
+
+  if [ "$csum" != "$correctCsum" ]; then
+    return 1
+  fi
+
+  return 0
+}
+
+runTest
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+runTest
+if [ $? -ne 0 ]; then
+  exit 1
+fi
