c6cbc8919783bad3789f752a59b4307398f6869f
Adam Watkins <adam@voskhod>
2018-04-16 15:20:41 +0100

    Expand testing

--- tests/test.sh
+++ tests/test.sh
@@ -8,10 +8,17 @@ url="https://raw.githubusercontent.com/stupidpupil/https-keyscript/master/tests/
 export CRYPTTAB_KEY="$passphrase:$url"
 export CRYPTTAB_NAME="TestDevice"
 export CRYPTTAB_TRIED=0
+export TESTING=1
 
 runTest()
 {
   output="$(busybox sh lib/cryptsetup/scripts/wget_or_ask)"
+  exitCode=$?
+
+  if [ "$exitCode" -ne 0 ];then
+    return "$exitCode"
+  fi
+
   csum="$(echo "$output" | sha256sum | cut -d " " -f1)"
 
   if [ "$csum" != "$correctCsum" ]; then
@@ -28,5 +35,17 @@ fi
 
 runTest
 if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+export CRYPTTAB_KEY="$passphrase/a:$url"
+runTest
+if [ $? -ne 42 ]; then
+  exit 1
+fi
+
+export CRYPTTAB_KEY="$passphrase:https://not.a.real.address.example"
+runTest
+if [ $? -ne 42 ]; then
   exit 1
 fi
