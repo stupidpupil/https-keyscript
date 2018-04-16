031eac05ca3ecf0c87a535f8c194040629ef6520
Adam Watkins <adam@voskhod>
2018-04-16 22:45:18 +0100

    wget Debian and GitHub to try to ensure SNI etc.

--- etc/initramfs-tools/hooks/wget.sh
+++ etc/initramfs-tools/hooks/wget.sh
@@ -7,19 +7,25 @@ esac
 
 copy_exec /usr/bin/wget
 
-LIB_PAT="\".*/lib/.*\""
-STRACED_LIBS=$(strace /usr/bin/wget --no-iri -q -O - https://www.debian.org 2>&1 | grep -o "$LIB_PAT")
+strace_and_copy_libs_for_url ()
+{
+  LIB_PAT="\".*/lib/.*\""
+  STRACED_LIBS=$(strace /usr/bin/wget --no-iri -q -O - "$1" 2>&1 | grep -o "$LIB_PAT")
 
-echo "$STRACED_LIBS" | while IFS= read -r line
-  do
-  # Strip the quotation marks
-  line="${line%\"}"
-  line="${line#\"}"
+  echo "$STRACED_LIBS" | while IFS= read -r line
+    do
+    # Strip the quotation marks
+    line="${line%\"}"
+    line="${line#\"}"
 
-  if [ -f "$line" ]
-  then
-    copy_exec "$line"
-  fi
-done
+    if [ -f "$line" ]
+    then
+      copy_exec "$line"
+    fi
+  done
+}
+
+strace_and_copy_libs_for_url "https://www.debian.org"
+strace_and_copy_libs_for_url "https://raw.githubusercontent.com/stupidpupil/https-keyscript/master/tests/fixtures/encrypted_keyfile"
 
 copy_exec /etc/ssl/certs/ca-certificates.crt
