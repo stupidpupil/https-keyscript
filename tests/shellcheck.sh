0fd335ee1ddccdb2dc12d6122d592f251ea5c802
Adam Watkins <adam@voskhod>
2018-04-17 18:33:31 +0100

    Exclude SC1091 'Not following'

--- tests/shellcheck.sh
+++ tests/shellcheck.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-SC_EXCLUDE="SC2181,SC2162"
+SC_EXCLUDE="SC2181,SC2162,SC1091"
 
 shellcheck -s sh --exclude="$SC_EXCLUDE" src/lib/cryptsetup/scripts/wget_or_ask \
   src/etc/initramfs-tools/hooks/*.sh \
