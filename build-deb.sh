93634a1284aceebee63331acda3ae40c76b6c150
Adam Watkins <adam@voskhod>
2018-04-17 17:51:51 +0100

    Improve permissions when building deb

--- build-deb.sh
+++ build-deb.sh
@@ -4,4 +4,8 @@ chmod u=rwx src/etc/initramfs-tools/hooks/*.sh
 chmod u=rwx src/etc/initramfs-tools/scripts/init-premount/*.sh
 chmod u=rwx src/lib/cryptsetup/scripts/wget_or_ask
 
+chmod og=rx src/etc/initramfs-tools/hooks/*.sh
+chmod og=rx src/etc/initramfs-tools/scripts/init-premount/*.sh
+chmod og=rx src/lib/cryptsetup/scripts/wget_or_ask
+
 dpkg-deb -b src https-keyscript.deb
