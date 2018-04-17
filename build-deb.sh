cceb340505157c3cf16fc41692efcf580c608ec4
Adam Watkins <adam@voskhod>
2018-04-17 16:42:58 +0100

    Attempt to do enough to get it working on Travis

--- build-deb.sh
+++ build-deb.sh
@@ -4,4 +4,4 @@ chmod u=rwx src/etc/initramfs-tools/hooks/*.sh
 chmod u=rwx src/etc/initramfs-tools/scripts/init-premount/*.sh
 chmod u=rwx src/lib/cryptsetup/scripts/wget_or_ask
 
-dpkg-deb -b src
+dpkg-deb -b src https-keyscript.deb
--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cp src/etc/initramfs-tools/hooks/*.sh /etc/initramfs-tools/hooks/
-chmod +x /etc/initramfs-tools/hooks/*.sh
-
-cp "src/etc/initramfs-tools/scripts/init-premount/networking.sh" "/etc/initramfs-tools/scripts/init-premount/networking.sh"
-chmod +x "/etc/initramfs-tools/scripts/init-premount/networking.sh"
-
-cp "src/ib/cryptsetup/scripts/wget_or_ask" "/lib/cryptsetup/scripts/wget_or_ask"
